452d4013d5d1363ac1e50ee25c5908083f4886e7
ichakran <ismail.chakrane@capgemini.com>
2024-11-13 17:10:12 +0100

    modify GenNearest

--- GenNearest.py
+++ GenNearest.py
@@ -20,33 +20,52 @@ class GenNeirest:
     def __init__(self, videoSkeTgt):
         self.videoSkeletonTarget = videoSkeTgt
 
+
     def generate(self, ske):
-            min_distance = float('inf')
-            closest_idx = -1
+        """ Generate an image from the skeleton """
+        min_distance = float('inf')
+        closest_frame_idx = -1
+        
+        for idx in range(self.videoSkeletonTarget.skeCount()):
+            tgt_ske = self.videoSkeletonTarget.ske[idx]
+            distance = ske.distance(tgt_ske)  # Compute distance between current skeleton and target skeleton
+            if distance < min_distance:
+                min_distance = distance
+                closest_frame_idx = idx
+        
+        # If no valid skeleton found, return a blank image
+        if closest_frame_idx == -1:
+            return np.ones((64, 64, 3), dtype=np.uint8)
+        
+        return self.videoSkeletonTarget.readImage(closest_frame_idx)
 
-            for i in range(self.videoSkeletonTarget.skeCount()):
-                target_ske = self.videoSkeletonTarget.ske[i]
-                dist = self.compute_skeleton_distance(ske, target_ske)
+    # def generate(self, ske):
+    #         min_distance = float('inf')
+    #         closest_idx = -1
 
-                if dist < min_distance:
-                    min_distance = dist
-                    closest_idx = i
+    #         for i in range(self.videoSkeletonTarget.skeCount()):
+    #             target_ske = self.videoSkeletonTarget.ske[i]
+    #             dist = self.compute_skeleton_distance(ske, target_ske)
 
-            if closest_idx == -1:
-                print("No close skeleton found.")
-                return np.zeros((128, 128, 3), dtype=np.uint8)
+    #             if dist < min_distance:
+    #                 min_distance = dist
+    #                 closest_idx = i
 
-            closest_image = self.videoSkeletonTarget.readImage(closest_idx)
-            return closest_image
+    #         if closest_idx == -1:
+    #             print("No close skeleton found.")
+    #             return np.zeros((128, 128, 3), dtype=np.uint8)
 
-    def compute_skeleton_distance(self, ske1, ske2):
-        ske1_array = ske1.__array__(reduced=True)
-        ske2_array = ske2.__array__(reduced=True)
+    #         closest_image = self.videoSkeletonTarget.readImage(closest_idx)
+    #         return closest_image
 
-        if ske1_array.shape != ske2_array.shape:
-            return float('inf')
+    # def compute_skeleton_distance(self, ske1, ske2):
+    #     ske1_array = ske1.__array__(reduced=True)
+    #     ske2_array = ske2.__array__(reduced=True)
 
-        distance = np.linalg.norm(ske1_array - ske2_array)
-        return distance
+    #     if ske1_array.shape != ske2_array.shape:
+    #         return float('inf')
+
+    #     distance = np.linalg.norm(ske1_array - ske2_array)
+    #     return distance
 
 
